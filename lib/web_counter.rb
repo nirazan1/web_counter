57079d11bdcc2eb0b9bae95a7fb20105f3a88db4
Nirajan Pokharel <nirajan@whitehatengineering.com>
2020-08-26 22:05:25 +0545

    Add method to count letter

--- lib/web_counter.rb
+++ lib/web_counter.rb
@@ -1,7 +1,13 @@
-class WebCounter
+require 'open-uri'
+require 'pry'
+require 'nokogiri'
 
-  def self.letter_count
-    return 'Ok'
+class WebCounter
+  LETTER_COUNT_BASE_URL = 'https://www.reuters.com'
+  BASE_DIVIDER = 100
+
+  def self.letter_count(letter, url=LETTER_COUNT_BASE_URL)
+    texts(url).join('').scan(/\w/).inject(Hash.new(0)){|h, c| h[c] += 1; h}[letter.downcase]/BASE_DIVIDER
   end
 
   def self.word_count
@@ -13,5 +19,14 @@ class WebCounter
   end
 
   private
+
+  def self.texts(url)
+    texts = html(url).xpath('.//text() | text()').map(&:inner_text).join(' ')
+    texts.scan(/\w+/).map(&:downcase)
+  end
+
+  def self.html(url)
+    Nokogiri::HTML(open(url))
+  end
 
 end
